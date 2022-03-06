8208ee507a86bc30624fddc2ecdfcb70e68ec23c
Soubhagya Samal <91544002+Soubhagya118@users.noreply.github.com>
2022-03-06 10:34:11 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Mobile Operating System</h1>
+    <ul>
+      <li>Android</li>
+      <li>Blackbery</li>
+      <li>iphone</li>
+      <li>Windows phone</li>
+    </ul>
+    <h1>Mobile Manufatures</h1>
+    <ul>
+      <li>Samsung</li>
+      <li>HTC</li>
+      <li>Micromax</li>
+      <li>Apple</li></ul>
+    </div>
+  );
+}
+
+export default App;
